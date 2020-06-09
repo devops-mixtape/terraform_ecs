bf966c79ab570168cac6b3a589965f0ed1adb3db
Nelson Wittwer <nelsonwittwer@gmail.com>
2020-06-08 20:46:11 -0400

    ECS Fargate example

---
+++ ecs.tf
@@ -0,0 +1,44 @@
+resource "aws_ecs_cluster" "hello_world" {
+  name = "hello_world"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello_world"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": 256,
+    "image": "devopsmixtape/hello_world_docker:latest",
+    "memory": 512,
+    "name": "hello_world",
+    "networkMode": "awsvpc",
+    "portMappings": [
+      {
+        "hostPort": 8080,
+        "protocol": "tcp",
+        "containerPort": 8080
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "hello_world" {
+  name               = "hello_world"
+  cluster            = "${aws_ecs_cluster.hello_world.id}"
+  task_definition    = "${aws_ecs_task_definition.hello_world.arn}"
+  desired_count      = 1
+  launch_type        = "FARGATE"
+
+  network_configuration {
+    subnets          = ["${aws_subnet.public.id}"]
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.http_access.id}"]
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.public.id}"
+}
+
+resource "aws_security_group" "http_access" {
+  name        = "HTTP access"
+  description = "Enables HTTP access"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
